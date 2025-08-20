cbddc2ff67ebf77cdc7f6c6f9006d8c07fb6d82e
nik1tosssa <realprimestels@gmail.com>
2025-08-20 14:38:40 +0300

    feat: Add assets for Daily Bonus Get screen
    This commit adds new image assets for the "Daily Bonus Get" screen:
    - `back_title.png`
    - `background.png`

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -4,6 +4,7 @@ import 'package:blessing_of_olympus/app/screens/splash_screeen/splash_screen.dar
 import 'package:blessing_of_olympus/app/screens/no_connection_screen/no_internet_connection_screen.dart';
 import 'package:blessing_of_olympus/app/screens/daily_bonus_screen/daily_bonus_screen.dart';
 import 'package:blessing_of_olympus/app/screens/shop_screen/shop_screen.dart';
+import 'package:blessing_of_olympus/app/screens/daily_bonus_get_screen/daily_bonus_get_screen.dart';
 
 
 class App extends StatelessWidget {
@@ -17,7 +18,7 @@ class App extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const HomeScreen(title: 'title'),
+      home: const HomeScreen(title: ''),
     );
   }
 }
